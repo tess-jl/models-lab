fb25387bcb85e1c550a814e64cbf7e5ef91ea8fb
Tess Lameyer <tjlameyer@gmail.com>
2019-12-04 14:30:19 -0800

    writeJSON test passing

--- __tests__/file-system-functions.test.js
+++ __tests__/file-system-functions.test.js
@@ -1,6 +1,6 @@
 const {
-  mkdirp
-//   writeJSON, 
+  mkdirp,
+  writeJSON 
 //   readDirectoryJSON, 
 //   updateJSON, 
 //   deleteFile
@@ -11,16 +11,23 @@ const fs = require('fs').promises;
 jest.mock('fs', () => ({
   promises: {
     mkdir: jest.fn(() => Promise.resolve('This would be the path')), 
+    writeFile: jest.fn(() => Promise.resolve('The file has been written'))
   }
 }));
-  
+
+
 describe('file system functions', () => {
-  describe('mkdirp function', () => {
-    it('make a directory and all parent directories', () => {
-      return mkdirp('./file.js')
-        .then(() => {
-          expect(fs.mkdir).toHaveBeenLastCalledWith('./file.js', { recursive: true });
-        });
-    });
+  it('make a directory and all parent directories (mkdirp function)', () => {
+    return mkdirp('./file.js')
+      .then(() => {
+        expect(fs.mkdir).toHaveBeenLastCalledWith('./file.js', { recursive: true });
+      });
   });
+  it('writes an object to a file (writeJSON function)', () => {
+    return writeJSON('./file.js', {})
+      .then(() => {
+        expect(fs.writeFile).toHaveBeenLastCalledWith('./file.js', '{}');
+      });
+  });
+
 });
