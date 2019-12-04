9d61d2c530ddace9394a7d58d68fc19f6d817db8
Tess Lameyer <tjlameyer@gmail.com>
2019-12-04 13:10:17 -0800

    scaffolding with added files for today

---
+++ __tests__/file-system-functions.test.js
@@ -0,0 +1,6 @@
+// mkdirp - make a directory and all parent directories
+// writeJSON - write an object to a file
+// readJSON - read an object from a file
+// readDirectoryJSON - read all files in a directory as objects
+// updateJSON - update a files JSON
+// deleteFile - delete a file
